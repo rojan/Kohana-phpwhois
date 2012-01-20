747d08ac6ff7dc3fd073df3a87deb1ee3dc1f493
rojan <rojansinha@gmail.com>
2012-01-20 11:19:41 +0545

    modified readme file and added license and documentation

--- classes/kohana/whois.php
+++ classes/kohana/whois.php
@@ -1,8 +1,14 @@
-<?php
+<?php defined('SYSPATH') OR die('No direct access allowed.');
 /**
- * 
+ * Kohana-whois is the wrapper for phpwhois library.
+ * find the latest version of phpwhois at http://sourceforge.net/projects/phpwhois/
+ *
+ * @package kohana-whois
+ * @author Rojan <rojansinha@gmail.com>
+ * @copyright (c) 2012 Rojan Sinha
+ * @license MIT
+ *
  **/
-
 
 class Kohana_Whois
 {
@@ -23,6 +29,13 @@ class Kohana_Whois
 		return Kohana_Whois::$_instance;
 	}
 
+	/**
+	 * Domain lookup
+	 *
+	 * @param	string	Domain name
+	 * @return	array	raw whois query
+	 *
+	 */
 	public function Lookup($domain)
 	{
 		return $this->_whois->Lookup($domain);
